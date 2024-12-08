883e1626d4be4fd79e67a87d21080c2e2bf0559a
hulxv <hulxxv@gmail.com>
2024-12-08 20:20:38 +0200

    docs; improve the docs of `pixel.h`

--- src/pixel.c
+++ src/pixel.c
@@ -1,11 +1,11 @@
 #include "pixel.h"
 #include "helpers.h"
-
+#include <stdint.h>
 /**
  * Creating a new pixel.
  */
 
-pixel *pixel_new(short red, short green, short blue)
+pixel *pixel_new(uint32_t red, uint32_t green, uint32_t blue)
 {
     UNIMPLEMENTED();
 }
@@ -17,13 +17,18 @@ pixel *pixel_new(short red, short green, short blue)
  *   I = 0.299*R + 0.587*G + 0.114*B
  */
 
-unsigned short pixel_to_grayscale(pixel px)
+uint32_t pixel_intensity(pixel px)
 {
     UNIMPLEMENTED();
 }
 
-/*
- * Converting the pixel to ascii character depends on its intensity.
+/**
+ * Converting the pixel to ascii character depends on its intensity (Grayscale value).
+ *
+ * You can find the index of the suitable ascii code using this formula:
+ *  index = I/255 * strlen(ASCII_CHARS)
+ *
+ * where I is the intensity of the pixel
  */
 
 char pixel_to_ascii(pixel px)
--- src/pixel.h
+++ src/pixel.h
@@ -1,28 +1,37 @@
 #ifndef _PIXEL_H
 #define _PIXEL_H
+#include <stdint.h>
+
+// There are 10 levels of brightness. It depends on the intensity (brightness) of the pixel.
+const char ASCII_CHARS[10] = "@\%#*+=-:. ";
 
 typedef struct
 {
-    short rgb[3];
+    uint32_t rgb[3];
 } pixel;
 
 /**
  * Creating a new pixel.
  */
 
-pixel *pixel_new(short red, short green, short blue);
+pixel *pixel_new(uint32_t red, uint32_t green, uint32_t blue);
 
 /**
- * Converting the pixel into gray scale.
+ * Getting the intensity level of a specific pixel.
  *
  * Formula:
- *   GrayScale = 0.299*R + 0.587*G + 0.114*B
+ *   I = 0.299*R + 0.587*G + 0.114*B
  */
 
-unsigned short pixel_grayscale(pixel px);
+uint32_t pixel_intensity(pixel px);
 
-/*
- * Converting the pixel to ascii character depends on its intensity.
+/**
+ * Converting the pixel to ascii character depends on its intensity (Grayscale value).
+ *
+ * You can find the index of the suitable ascii code using this formula:
+ *  Index = I/255 * strlen(ASCII_CHARS)
+ *
+ * where I is the intensity of the pixel
  */
 
 char pixel_to_ascii(pixel px);
