7d312146e4f8cd94cab920ed5b78fae54637ad46
hulxv <hulxxv@gmail.com>
2024-12-15 00:53:12 +0200

    fix: flicker using double buffering

--- src/main.c
+++ src/main.c
@@ -9,39 +9,53 @@
 #include <unistd.h>
 
 #include "video.h"
+#include "pixel.h"
 #include "helpers.h"
+#include "specs.h"
 
 #define FRAME_DELAY 33333 // Microseconds (1/30 second)
 
 static void ascii_save(unsigned char *buf, int wrap, int xsize, int ysize, char *filename)
 {
     FILE *file = fopen(filename, "wb");
-    const char *ascii_map = " .:-=+*%#@";
-    int ascii_len = strlen(ascii_map);
 
     for (int y = 0; y < ysize; y++)
     {
         for (int x = 0; x < xsize; x++)
         {
-            // Map pixel intensity (0-255) to an ASCII character
-            unsigned char pixel_value = buf[y * wrap + x];
-            char ascii_char = ascii_map[(pixel_value * (ascii_len - 1)) / 255];
+            pixel px = pixel_new(
+                buf[y * wrap + x],
+                buf[y * wrap + x + 1],
+                buf[y * wrap + x + 2]);
+            char ascii_char = pixel_to_ascii(px);
+
             fputc(ascii_char, file);
             fputc(ascii_char, file);
             fputc(ascii_char, file);
             // printf("f");
         }
-        fputc('\n', file); // New line for each row
+        fputc('\n', file);
     }
 
-    // fwrite(file_buf, xsize * ysize, xsize * ysize, file);
     fclose(file);
 }
 
-static void player(const char *prefix, int width, int height)
+static void player(const char *prefix, int width, int height, int frames_count)
 {
+    int term_rows, term_cols;
+    getmaxyx(stdscr, term_rows, term_cols); // Get terminal size
 
-    for (int i = 1; i <= 450; i++)
+    int offset_y = (term_rows - height) / 2;
+    int offset_x = (term_cols - width) / 2;
+
+    WINDOW *frame_pad = newpad(height, width);
+    if (!frame_pad)
+    {
+        fprintf(stderr, "Error: Could not create frame pad\n");
+        return;
+    }
+
+    for (int i = 1; i <= frames_count; i++)
     {
         char filename[128];
         snprintf(filename, sizeof(filename), "%s-%i.ascii", prefix, i);
@@ -54,20 +68,25 @@ static void player(const char *prefix, int width, int height)
             break; // Exit loop on failure
         }
 
-        char ch;
-        while ((ch = fgetc(file)) != EOF)
+        werase(frame_pad);
+
+        char line[width + 1];
+        for (int y = 0; y < height; y++)
         {
-            printw("%c", ch);
+            if (fgets(line, sizeof(line), file) == NULL)
+                break; 
+
+            mvwprintw(frame_pad, y, 0, "%.*s", width, line);
         }
 
-        fclose(file); // Close the file
+        fclose(file); 
 
-        // Pause for the frame delay
-        refresh();           // Refresh the screen to display the frame
-        usleep(FRAME_DELAY); // Delay in microseconds (e.g., 33ms for ~30fps)
+        prefresh(frame_pad, 0, 0, offset_y, offset_x, offset_y + height - 1, offset_x + width - 1);
 
-        clear(); // Clear the screen for the next frame
+        usleep(FRAME_DELAY); 
     }
+
+    delwin(frame_pad);
 }
 
 static int decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt, const char *filename)
@@ -117,6 +136,14 @@ int main(int argc, char **argv)
     outfilename = argv[2];
 
     video *vid = video_new(filename, outfilename);
+    AVRational framerate = vid->codec_ctx->framerate;
+
+    float fps = framerate.num / framerate.den;
+    float duration = vid->fmt_ctx->duration / (float)AV_TIME_BASE;
+    int frames_count = (int)fps * (int)duration;
+    specs *specs = specs_new((int)fps * (int)duration, fps, duration, vid->video_stream->codecpar->width, vid->video_stream->codecpar->height);
+
+    INFO("Creating specs file, content={%s}", specs_serialize(specs));
 
     video_decode_frames(vid, decode);
     // Flush the decoder
@@ -126,7 +153,7 @@ int main(int argc, char **argv)
     noecho();    // Don't echo input characters
     curs_set(0); // Hide the cursor
     INFO("w=%i, h=%i", vid->codec_ctx->width, vid->codec_ctx->height);
-    player(outfilename, vid->codec_ctx->width, vid->codec_ctx->height);
+    player(outfilename, vid->codec_ctx->width * 4, vid->codec_ctx->height, frames_count);
     endwin();
     // Clean up
     video_clean_up(vid);
--- src/pixel.c
+++ src/pixel.c
@@ -5,18 +5,13 @@
 #include "helpers.h"
 
 // create a new pixel
-pixel *pixel_new(uint16_t red, uint16_t green, uint16_t blue)
+pixel pixel_new(uint16_t red, uint16_t green, uint16_t blue)
 {
-    pixel *newpixel = (pixel *)malloc(sizeof(pixel)); // allocate memory using malloc function for new pixel with using red and green and blue colours
+    pixel newpixel; // allocate memory using malloc function for new pixel with using red and green and blue colours
 
-    if (newpixel == NULL) // if memory allocation fails then return to NULL
-    {
-        return NULL;
-    }
-
-    newpixel->rgb[0] = red;   // set red colour
-    newpixel->rgb[1] = green; // set green colour
-    newpixel->rgb[2] = blue;  // set blue colour
+    newpixel.r = red;   // set red colour
+    newpixel.g = green; // set green colour
+    newpixel.b = blue;  // set blue colour
 
     return newpixel; // return pixel
 }
@@ -26,7 +21,7 @@ uint16_t pixel_intensity(pixel px)
 {
     uint16_t intensity; // defining intensity in  this function as local
 
-    intensity = (0.299 * px.rgb[0] + 0.587 * px.rgb[1] + 0.114 * px.rgb[2]); // getting intensity using red, green and blue colours
+    intensity = (0.299 * px.r + 0.587 * px.g + 0.114 * px.b); // getting intensity using red, green and blue colours
 
     return intensity; // return intensity as a value
 }
@@ -34,7 +29,7 @@ uint16_t pixel_intensity(pixel px)
 // converting each pixel into ascii characters
 char pixel_to_ascii(pixel px)
 {
-    const char ASCII_CHARS[] = "@\%#*+=-:. \0";
+    static char ASCII_CHARS[] = " .:-=+*%#@";
 
     uint16_t intensity, index; // defining intensity and index in this function as locals
 
@@ -42,7 +37,7 @@ char pixel_to_ascii(pixel px)
 
     uint16_t ascii_length = strlen(ASCII_CHARS) - 1; // defining Ascii length as the string length of ASCII characters minus 1
 
-    index = (ascii_length)-intensity / 255.0 * (ascii_length); // calculating index using the formula
+    index = intensity / 255.0 * (ascii_length); // calculating index using the formula
 
     return ASCII_CHARS[index]; // return ASCII characters of the index
 }
