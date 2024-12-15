78f8e589690330e722aff85464738100eb459abd
hulxv <hulxxv@gmail.com>
2024-12-15 02:02:03 +0200

    refactor: improve `frame`
    - make `frame` take array of video buffer instead of array of `pixel`s
    - remove `frame_to_ascii`: it's useless

--- src/frame.h
+++ src/frame.h
@@ -6,26 +6,27 @@
 
 typedef struct
 {
-  uint64_t id; // the ID of the frame, its order in the video
-  uint32_t height;
-  uint32_t width;
-  pixel pixels[]; // Array of pixels for the frame.
+  uint64_t id;
+  int wrap;
+  int width;
+  int height;
+  unsigned char *buf;
 } frame;
 
 /*
  * Creating a new frame.
  */
-frame *frame_new(uint64_t id, pixel pixels[], unsigned int width, unsigned int height);
-
-/*
- * Converting the frame into Ascii Art.
- */
-char *frame_to_ascii(frame *frame);
+frame *frame_new(uint64_t id, uint8_t *buf, int wrap, int width, int height);
 
 /*
  * Writing the frame that converted into Ascii Art to the path of the
  * destination file.
  */
-int frame_ascii_write_to(frame *frame, char* filename);
+int frame_ascii_write_to(frame *frame, char *filename);
+
+/**
+ * Clean up the frame 
+ */
+void frame_free(frame *f);
 
 #endif
