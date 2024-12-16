50823ba19279790ff422c02af4759b4b5178a66f
hulxv <hulxxv@gmail.com>
2024-12-16 20:25:15 +0200

    feat: stuck the player when the dimensions of terminal don't fit the video

--- src/player.c
+++ src/player.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
+#include <signal.h>
 
 #include "player.h"
 #include "helpers.h"
@@ -17,7 +18,6 @@ static int print_frame(char *filename, WINDOW *frame_pad, int width, int height)
     }
 
     werase(frame_pad);
-
     char line[width + 1];
     for (int y = 0; y < height; y++)
     {
@@ -42,32 +42,73 @@ video_player *video_player_new(char *src, specs *specs)
 int video_player_run(video_player *player)
 
 {
+
     int delay = 1000000 / player->specs->fps;
 
-    int term_rows, term_cols;
     int width = player->specs->width * 4, height = player->specs->height;
-    getmaxyx(stdscr, term_rows, term_cols); // Get terminal size
 
-    int offset_y = (term_rows - height) / 2;
-    int offset_x = (term_cols - width) / 2;
+    INFO("Video dimensions: width = %i, height = %i\n", width, height);
 
+    int offset_y;
+    int offset_x;
+
+    int term_rows, term_cols;
+    int prev_term_rows, prev_term_cols;
+
+    getmaxyx(stdscr, prev_term_rows, prev_term_cols);
     WINDOW *frame_pad = newpad(height, width);
+
     if (!frame_pad)
     {
         ERROR(" Could not create frame pad\n");
         return -1;
     }
 
+    float fps = player->specs->fps;
+    uint32_t frames_count = player->specs->frames_count;
+
     for (int i = 1; i <= player->specs->frames_count; i++)
     {
+        werase(frame_pad);
+        do
+        {
+            getmaxyx(stdscr, term_rows, term_cols);
+
+            offset_y = (term_rows - height) / 2;
+            offset_x = (term_cols - width) / 2;
+
+            if (offset_y >= 0 && offset_x >= 0)
+            {
+                break;
+            }
+            else
+            {
+
+                clear();
+                mvprintw(term_rows / 2, (term_cols - 30) / 2, "Resize terminal to %dx%d", width, height);
+                mvprintw(term_rows / 2 + 1, (term_cols - 30) / 2, "Current: %dx%d", term_cols, term_rows);
+                refresh();
+
+                usleep(10000);
+            }
+        } while (1);
+
         char filename[128];
+
         snprintf(filename, sizeof(filename), "%s%i.ascii", player->src, i);
 
         print_frame(filename, frame_pad, width, height);
-
+        if (term_rows != prev_term_rows || term_cols != prev_term_cols)
+        {
+            clear();
+            refresh();
+        }
         prefresh(frame_pad, 0, 0, offset_y, offset_x, offset_y + height - 1, offset_x + width - 1);
 
         usleep(delay);
+
+        prev_term_rows = term_rows;
+        prev_term_cols = term_cols;
     }
 
     delwin(frame_pad);
